ae3e8142062e5641b17ab31af28ca0f830cc239a
Jay7221 <jns29072003@gmail.com>
2023-03-16 13:43:20 +0530

    Hosting

--- medicine/models.py
+++ medicine/models.py
@@ -3,9 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Medicine(models.Model):
-    type = models.CharField(max_length=200)
-    name = models.CharField(max_length=500)
-    price = models.DecimalField(max_digits=10, decimal_places=3)
-    therauptic_category = models.CharField(max_length=200)
-    disease = models.CharField(max_length=200)
+    name = models.CharField(max_length=500, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(max_length=500, null=True, blank=True)
+    price = models.CharField(max_length=100, null=True, blank=True)
+    manufacturer = models.CharField(max_length=200, null=True, blank=True)
+    type = models.CharField(max_length=200, null=True, blank=True)
+    quantity = models.CharField(max_length=100, null=True, blank=True)
+    dosage = models.CharField(max_length=100, null=True, blank=True)
+    substance = models.CharField(max_length=100, null=True, blank=True)
+    therauptic_category = models.CharField(max_length=200, blank=True, null=True)
+    disease = models.CharField(max_length=200, blank=True, null=True)
+    
